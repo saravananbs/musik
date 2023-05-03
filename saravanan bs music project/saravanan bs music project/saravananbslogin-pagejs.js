332550a0e9ec604a2626e372e7176a0a1b75d969
saravananbs <132475341+saravananbs@users.noreply.github.com>
2023-05-03 20:35:24 +0530

    Add files via upload

---
+++ saravanan bs music project/saravanan bs music project/saravananbslogin-pagejs.js
@@ -0,0 +1,13 @@
+const form = document.querySelector('form');
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  
+  if (username.value === '' || password.value === '') {
+    alert('Please fill in all fields');
+  } else  {
+    retur(C:\Users\HP\OneDrive\Desktop\rahulproject.html);
+  } 
+});
